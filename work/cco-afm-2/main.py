f97facd2a873312a0798a54032fddbb992d57ddd
Junjie Yang <yangjunjie0320@gmail.com>
2025-11-21 23:32:39 -0800

    add cuda kernel for vr_dot_dm

---
+++ work/cco-afm-2/main.py
@@ -0,0 +1,67 @@
+import cupy as cp
+
+device = cp.cuda.Device()
+print(f"Using GPU device {device.id}")
+
+mem_info = cp.cuda.runtime.memGetInfo()
+print(f"Free memory: {mem_info[0] / (1024**3):.2f} GB")
+print(f"Total memory: {mem_info[1] / (1024**3):.2f} GB")
+
+import pyscf, gpu4pyscf
+from pyscf.pbc import gto
+import gpu4pyscf.pbc
+
+import sys, pathlib
+poscar_path = pathlib.Path("/resnick/groups/changroup/members/junjiey/occri/packages/")
+poscar_path = poscar_path / "cuprate_parent_state_data/01_crystal_geometry/CCO/CCO-AFM-frac.vasp"
+assert poscar_path.exists()
+
+path = str(poscar_path)
+print(f"poscar_path: {path}")
+
+from libdmet.utils.iotools import read_poscar
+pcell = read_poscar(path)
+pcell.basis = 'gth-dzvp-molopt-sr'
+pcell.pseudo = "gth-hf-rev"
+pcell.ke_cutoff = 200
+pcell.exp_to_discard = 0.1
+pcell.verbose = 5
+pcell.build()
+
+kpts = pcell.make_kpts([2, 2, 1])
+
+alph_label = ["0 Cu 3dx2-y2"]
+beta_label = ["1 Cu 3dx2-y2"]
+alph_ix = pcell.search_ao_label(alph_label)
+beta_ix = pcell.search_ao_label(beta_label)
+
+from gpu4pyscf.pbc.scf import KRHF, KUHF
+mf = KUHF(pcell, kpts)
+mf.verbose = 5
+mf.conv_tol = 1e-6
+mf.max_cycle = 50
+mf.diis_space = 4
+mf.exxdiv = None
+dm0 = mf.get_init_guess(key='minao')
+
+dm0[0, :, alph_ix, alph_ix] *= 2.0
+dm0[1, :, beta_ix, beta_ix] *= 2.0
+dm0[0, :, beta_ix, beta_ix] *= 0.0
+dm0[1, :, alph_ix, alph_ix] *= 0.0
+
+from fftdf import FFTDF
+mf.with_df = FFTDF(pcell, kpts, with_occri=True, use_gpu=True)
+mf.with_df.blksize = 32
+mf.with_df._mo_is_missing = "fftdf"
+
+mf.kernel(dm0)
+ene_sol = mf.e_tot
+
+from gpu4pyscf.pbc.df.fft import FFTDF
+mf.with_df = FFTDF(pcell, kpts)
+mf.kernel(dm0)
+ene_ref = mf.e_tot
+
+print("ene_ref = %12.6f" % ene_ref)
+print("ene_sol = %12.6f" % ene_sol)
+print("error   = %6.2e" % abs(ene_ref - ene_sol))
---
+++ work/cco-afm-2/run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#SBATCH --reservation=changroup-h100-node-1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=gpu4pyscf-occri
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=10gb
+#SBATCH --time=08:00:00
+
+echo "SLURMD_NODENAME = $SLURMD_NODENAME"
+echo "Start time = $(date)"
+echo "CUDA_VISIBLE_DEVICES = $CUDA_VISIBLE_DEVICES"
+
+source $HOME/anaconda3/bin/activate gpu4pyscf-occri
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK;
+export MKL_NUM_THREADS=4;
+export OPENBLAS_NUM_THREADS=4;
+export PYSCF_MAX_MEMORY=$((SLURM_MEM_PER_CPU * SLURM_CPUS_PER_TASK))
+
+echo OMP_NUM_THREADS = $OMP_NUM_THREADS
+echo MKL_NUM_THREADS = $MKL_NUM_THREADS
+echo OPENBLAS_NUM_THREADS = $OPENBLAS_NUM_THREADS
+echo PYSCF_MAX_MEMORY = $PYSCF_MAX_MEMORY
+
+export TMP=/resnick/scratch/yangjunjie/
+export TMPDIR=$TMP/$SLURM_JOB_NAME/$SLURM_JOB_ID/
+export PYSCF_TMPDIR=$TMPDIR
+
+mkdir -p $TMPDIR
+echo TMPDIR       = $TMPDIR
+echo PYSCF_TMPDIR = $PYSCF_TMPDIR
+ln -s $PYSCF_TMPDIR tmp
+
+export PYTHONPATH=/home/junjiey/work/occri/occri-main/src/:$PYTHONPATH
+export PYTHONPATH=/home/junjiey/work/occri/packages/libdmet2-main/:$PYTHONPATH
+
+# nsys profile --stats=true python main.py
+# python main.py
+nsys profile \
+  --stats=true \
+  --trace=cuda,nvtx,osrt \
+  --python-backtrace=cuda \
+  --python-sampling=true \
+  --output=profile \
+  --force-overwrite=true \
+  python main.py
+
+echo "End time = $(date)"
